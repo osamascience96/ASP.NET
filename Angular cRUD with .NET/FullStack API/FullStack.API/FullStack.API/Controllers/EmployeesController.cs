206271ec388c94ea8f7de4e91b39ca4d960266e9
osamascience96 <osamahu96@gmail.com>
2022-07-12 10:03:32 +0500

    Endpoint to get employee by id

--- Angular cRUD with .NET/FullStack API/FullStack.API/FullStack.API/Controllers/EmployeesController.cs
+++ Angular cRUD with .NET/FullStack API/FullStack.API/FullStack.API/Controllers/EmployeesController.cs
@@ -34,5 +34,19 @@ namespace FullStack.API.Controllers
 
             return Ok(employee);
         }
+
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> GetEmployee([FromRoute] Guid id)
+        {
+            var employee = await _fullStackDbContext.Employees.FirstOrDefaultAsync(x => x.Id == id);
+
+            if(employee == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(employee);
+        }
     }
 }
