66d95316aafcd6e1c328bf73c708cace7216f34b
osamascience96 <osamahu96@gmail.com>
2022-07-12 12:59:28 +0500

    Added Delete Endpoint to delete the employee

--- Angular cRUD with .NET/FullStack API/FullStack.API/FullStack.API/Controllers/EmployeesController.cs
+++ Angular cRUD with .NET/FullStack API/FullStack.API/FullStack.API/Controllers/EmployeesController.cs
@@ -70,5 +70,22 @@ namespace FullStack.API.Controllers
 
             return Ok(empl);
         }
+
+        [HttpDelete]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> DeleteEmployee([FromRoute] Guid id)
+        {
+            var employee = await _fullStackDbContext.Employees.FindAsync(id);
+
+            if(employee == null)
+            {
+                return NotFound();
+            }
+
+            _fullStackDbContext.Remove(employee);
+            await _fullStackDbContext.SaveChangesAsync();
+
+            return Ok();
+        }
     }
 }
